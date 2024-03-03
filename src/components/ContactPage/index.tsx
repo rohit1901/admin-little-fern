72d7ceb484284f360672f987d1c6fa56a6ab15ff
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-03-03 15:41:19 +0100

    CHORE: added elemValue to FormElement for null checks

--- src/components/ContactPage/index.tsx
+++ src/components/ContactPage/index.tsx
@@ -43,62 +43,63 @@ const ContactPageComponent = ({contactPageData}: ContactPageProps) => {
                                 src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"></iframe>
                         <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md dark:bg-gray-800">
                             <div className="lg:w-1/2 px-6">
-                                {contactInformation?.address &&
-                                    <LFFormElement labelValue="Address" labelName="contact-address">
-                                        <Textarea id="contact-address" placeholder="Address"
-                                                  value={contactInformation?.address} required className="h-text-area"
-                                                  onChange={(event) => setContactInformationAddress(event.target.value)}/>
-                                    </LFFormElement>}
+                                <LFFormElement labelValue="Address" labelName="contact-address"
+                                               elemValue={contactInformation?.address}>
+                                    <Textarea id="contact-address" placeholder="Address"
+                                              value={contactInformation?.address} required className="h-text-area"
+                                              onChange={(event) => setContactInformationAddress(event.target.value)}/>
+                                </LFFormElement>
                             </div>
                             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                                {contactInformation?.phone &&
-                                    <LFFormElement labelValue="Phone" labelName="contact-phone">
-                                        <TextInput id="contact-phone" placeholder="Phone"
-                                                   value={contactInformation?.phone} required
-                                                   onChange={(event) => setContactInformationPhone(event.target.value)}/>
-                                    </LFFormElement>}
-                                {contactInformation?.email &&
-                                    <LFFormElement labelValue="Email" labelName="contact-email">
-                                        <TextInput id="contact-email" placeholder="Email"
-                                                   value={contactInformation?.email} required
-                                                   onChange={(event) => setContactInformationEmail(event.target.value)}/>
-                                    </LFFormElement>}
+                                <LFFormElement labelValue="Phone" labelName="contact-phone"
+                                               elemValue={contactInformation?.phone}>
+                                    <TextInput id="contact-phone" placeholder="Phone"
+                                               value={contactInformation?.phone} required
+                                               onChange={(event) => setContactInformationPhone(event.target.value)}/>
+                                </LFFormElement>
+                                <LFFormElement labelValue="Email" labelName="contact-email"
+                                               elemValue={contactInformation?.email}>
+                                    <TextInput id="contact-email" placeholder="Email"
+                                               value={contactInformation?.email} required
+                                               onChange={(event) => setContactInformationEmail(event.target.value)}/>
+                                </LFFormElement>
                             </div>
                         </div>
                     </div>
                     {/* Properties */}
                     <div
                         className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 dark:bg-gray-800">
-                        {contactInformation?.hero?.tagline &&
-                            <LFFormElement labelValue="Tagline" labelName="contact-hero-tagline">
-                                <TextInput id="contact-hero-tagline" placeholder="Tagline for the Hero Block"
-                                           value={contactInformation?.hero?.tagline} required
-                                           onChange={(event) => setHeroTagline(event.target.value)}/>
-                            </LFFormElement>}
-                        {contactInformation?.hero?.headline &&
-                            <LFFormElement labelValue="Headline" labelName="contact-hero-headline">
-                                <TextInput id="contact-hero-headline" placeholder="Headline for the Hero Block"
-                                           value={contactInformation?.hero?.headline} required
-                                           onChange={(event) => setHeroHeadline(event.target.value)}/>
-                            </LFFormElement>}
-                        {contactInformation?.hero?.text &&
-                            <LFFormElement labelValue="Text" labelName="contact-hero-text">
-                                <Textarea id="contact-hero-text" placeholder="Text for the Hero Block"
-                                          className="h-text-area"
-                                          value={contactInformation?.hero?.text} required
-                                          onChange={(event) => setHeroText(event.target.value)}/>
-                            </LFFormElement>}
-                        {textBlock?.headline && <LFFormElement labelValue="Title" labelName="contact-events-title">
+                        <LFFormElement labelValue="Tagline" labelName="contact-hero-tagline"
+                                       elemValue={contactInformation?.hero?.tagline}>
+                            <TextInput id="contact-hero-tagline" placeholder="Tagline for the Hero Block"
+                                       value={contactInformation?.hero?.tagline} required
+                                       onChange={(event) => setHeroTagline(event.target.value)}/>
+                        </LFFormElement>
+                        <LFFormElement labelValue="Headline" labelName="contact-hero-headline"
+                                       elemValue={contactInformation?.hero?.headline}>
+                            <TextInput id="contact-hero-headline" placeholder="Headline for the Hero Block"
+                                       value={contactInformation?.hero?.headline} required
+                                       onChange={(event) => setHeroHeadline(event.target.value)}/>
+                        </LFFormElement>
+                        <LFFormElement labelValue="Text" labelName="contact-hero-text"
+                                       elemValue={contactInformation?.hero?.text}>
+                            <Textarea id="contact-hero-text" placeholder="Text for the Hero Block"
+                                      className="h-text-area"
+                                      value={contactInformation?.hero?.text} required
+                                      onChange={(event) => setHeroText(event.target.value)}/>
+                        </LFFormElement>
+                        <LFFormElement labelValue="Title" labelName="contact-events-title"
+                                       elemValue={textBlock?.headline}>
                             <TextInput id="contact-events-title" placeholder="Title"
                                        value={textBlock?.headline} required
                                        onChange={(event) => setTextBlockHeadline(event.target.value)}/>
-                        </LFFormElement>}
-                        {textBlock?.text &&
-                            <LFFormElement labelValue="Description" labelName="contact-events-description">
-                                <Textarea id="contact-events-description" placeholder="Description"
-                                          value={textBlock?.text} required className="h-text-area"
-                                          onChange={(event) => setTextBlockText(event.target.value)}/>
-                            </LFFormElement>}
+                        </LFFormElement>
+                        <LFFormElement labelValue="Description" labelName="contact-events-description"
+                                       elemValue={textBlock?.text}>
+                            <Textarea id="contact-events-description" placeholder="Description"
+                                      value={textBlock?.text} required className="h-text-area"
+                                      onChange={(event) => setTextBlockText(event.target.value)}/>
+                        </LFFormElement>
                     </div>
                 </LFFormSection>
             </LFForm>
--- src/components/GalleryPage/GalleryTextBlock.tsx
+++ src/components/GalleryPage/GalleryTextBlock.tsx
@@ -10,14 +10,16 @@ const GalleryTextBlock = () => {
     return <Fragment>
         <LFFormSection sectionTitle={'Text Block'}>
             <div className="sm:w-1/2 px-4">
-                <LFFormElement labelValue='Headline' labelName='text-block-headline'>
+                <LFFormElement labelValue='Headline' labelName='text-block-headline'
+                               elemValue={galleryPageData?.textBlock?.headline}>
                     <TextInput id="text-block-headline" placeholder="Headline for the Hero Block"
                                value={galleryPageData?.textBlock?.headline} required
                                onChange={(event) => setTextBlockHeadline(event.target.value)}/>
                 </LFFormElement>
             </div>
             <div className="sm:w-1/2 px-4">
-                <LFFormElement labelValue='Text' labelName='text-block-text'>
+                <LFFormElement labelValue='Text' labelName='text-block-text'
+                               elemValue={galleryPageData?.textBlock?.text}>
                     <TextInput id="text-block-text" placeholder="Text for the Text Block"
                                value={galleryPageData?.textBlock?.text} required
                                onChange={(event) => setTextBlockText(event.target.value)}/>
