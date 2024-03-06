7efff4c7329d16a4d78c0847bccf61336c8293d8
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-03-06 20:31:31 +0100

    WIP: icons

--- src/components/LFSidebar.tsx
+++ src/components/LFSidebar.tsx
@@ -1,25 +1,28 @@
 'use client';
 
 import {Sidebar, SidebarCollapse, SidebarItem, SidebarItemGroup, SidebarItems} from 'flowbite-react';
-import {BiSolidHelpCircle} from 'react-icons/bi';
+import {BiSolidDashboard, BiSolidHelpCircle, BiSolidPhoneCall} from 'react-icons/bi';
 import {HiChartPie, HiOfficeBuilding} from 'react-icons/hi';
 import {HiCamera, HiHome, HiMiniDocument, HiMiniInformationCircle, HiPhone, HiUserGroup} from "react-icons/hi2";
 import {usePathname} from "next/navigation";
+import {TbUsersGroup} from "react-icons/tb";
+import {IoDocuments} from "react-icons/io5";
+import {RiGalleryFill, RiHomeHeartFill} from "react-icons/ri";
 
 const SidebarClasses = "fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full " +
     "bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
 const pages = [{
-    name: 'Home', href: '/website-pages/Home', icon: HiHome,
+    name: 'Home', href: '/website-pages/Home', icon: RiHomeHeartFill,
 }, {
     name: 'About', href: '/website-pages/About', icon: HiMiniInformationCircle,
 }, {
-    name: 'Gallery', href: '/website-pages/Gallery', icon: HiCamera,
+    name: 'Gallery', href: '/website-pages/Gallery', icon: RiGalleryFill,
 }, {
     name: 'Programs', href: '/programs/play-group', icon: HiOfficeBuilding,
 }, {
-    name: 'Contact', href: '/website-pages/Contact', icon: HiPhone,
+    name: 'Contact', href: '/website-pages/Contact', icon: BiSolidPhoneCall,
 }, {
-    name: 'Parents', href: '/website-pages/Parents', icon: HiUserGroup,
+    name: 'Parents', href: '/website-pages/Parents', icon: TbUsersGroup,
 }]
 const setActive = (href: string, slug: string | null) => {
     // set active to true if the slug includes '/programs'
@@ -34,10 +37,10 @@ const LFSidebar = () => {
         className={SidebarClasses}>
         <SidebarItems className="sidebar-items">
             <SidebarItemGroup>
-                <SidebarItem href="/" icon={HiChartPie} active={setActive('/', slug)}>
+                <SidebarItem href="/" icon={BiSolidDashboard} active={setActive('/', slug)}>
                     Dashboard
                 </SidebarItem>
-                <SidebarCollapse icon={HiMiniDocument} label="Pages" open>
+                <SidebarCollapse icon={IoDocuments} label="Pages" open>
                     {pages?.map((page) => (<SidebarItem href={page.href} key={page.name}
                                                         icon={page.icon}
                                                         className='text-ellipsis overflow-hidden text-sm'
