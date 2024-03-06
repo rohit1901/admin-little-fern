1a2125d2136d1e989272919b3f95917dfb3c696c
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-03-06 11:53:19 +0100

    Loader (#6)

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -7,38 +7,39 @@ import LFSidebar from "@admin/components/LFSidebar";
 import {PropsWithChildren} from "react";
 import Image from "next/image";
 import {getImageUrl} from "@admin/lib";
+import Loader from "@admin/components/Loader";
 
 export default function LoginButton({children}: PropsWithChildren) {
-    const {data: session} = useSession();
-    if (session && session.user) {
-        return (
-            <Flowbite theme={{theme: customTheme}}>
-                <section>
-                    <LFNavbar/>
-                    <LFSidebar/>
-                    {children}
-                </section>
-            </Flowbite>
-        );
+    const {status} = useSession();
+
+    if (status === "loading") {
+        return <Loader/>
     }
-    return (
-        <section className="bg-white dark:bg-gray-800">
-            <Card className="max-w-sm mx-auto py-6 my-12">
-                <Image alt="Little Fern Logo"
-                       height="96"
-                       src={getImageUrl('/images/bright-logo.png')}
-                       width="96"
-                       className="mb-3 mx-auto"/>
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                    Sign in to your account
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400 text-center">
-                    You will be redirected to a secure sign in page where you can sign in with your AWS Cognito account.
-                </p>
-                <Button onClick={() => signIn()}>
-                    Sign in
-                </Button>
-            </Card>
-        </section>
-    );
+    if (status === "unauthenticated") return (<section className="bg-white dark:bg-gray-800">
+        <Card className="max-w-sm mx-auto py-6 my-12">
+            <Image alt="Little Fern Logo"
+                   height="96"
+                   src={getImageUrl('/images/bright-logo.png')}
+                   width="96"
+                   className="mb-3 mx-auto"/>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+                Sign in to your account
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400 text-center">
+                You will be redirected to a secure sign in page where you can sign in with your AWS Cognito account.
+            </p>
+            <Button onClick={() => signIn()}>
+                Sign in
+            </Button>
+        </Card>
+    </section>);
+    if (status === "authenticated") {
+        return (<Flowbite theme={{theme: customTheme}}>
+            <section>
+                <LFNavbar/>
+                <LFSidebar/>
+                {children}
+            </section>
+        </Flowbite>);
+    }
 }
