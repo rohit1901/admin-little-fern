7828426494ecb0ec652c5228bbb23c58a6c0e21f
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-03-20 21:03:23 +0100

    Feature/custom auth UI (#17)
    * Auth0 authentication
    
    * login screen styling
    
    * Navbar cosmetic changes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,15 @@
 import NextAuth from "next-auth"
-import CognitoProvider from "next-auth/providers/cognito";
+import Auth0Provider from "next-auth/providers/auth0";
 
 export const authOptions = {
-    providers: [CognitoProvider({
-        clientId: process.env.NEXT_PUBLIC_AWS_USER_POOL_APP_CLIENT_ID,
-        clientSecret: process.env.NEXT_PUBLIC_AWS_USER_POOL_APP_CLIENT_SECRET,
-        issuer: process.env.NEXT_PUBLIC_AUTH_ISSUER,
-        region: process.env.NEXT_PUBLIC_AWS_REGION,
-        client: {
-            token_endpoint_auth_method: "none"
-        }
-    })]
+    providers: [
+        Auth0Provider({
+            clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
+            clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET,
+            issuer: process.env.NEXT_PUBLIC_AUTH0_ISSUER,
+
+        })
+    ]
 }
 
 export default NextAuth(authOptions)
