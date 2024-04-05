00d3f8cd902318f727e2c1bcd74e2eb45b00235d
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-04-05 21:48:56 +0200

    Removed unnecessary data check

--- src/pages/api/reviews/get.ts
+++ src/pages/api/reviews/get.ts
@@ -5,10 +5,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const rawData = await fetch(getGoogleMapsUrl(process.env.PLACE_ID))
         const mapsData = await rawData.json()
-        if (!mapsData.result) {
-            res.status(500).json({message: 'Error fetching Google Maps data',});
-            return;
-        }
         res.status(200).json({message: 'success', body: mapsData.result});
     } catch (e) {
         console.error("Error fetching home page data", e)
